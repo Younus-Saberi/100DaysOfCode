e05689e0da0cff030a62502c6249c10339b92520
Younus-Saberi <younussaberi@gmail.com>
2023-05-10 17:12:04 +0530

    added pattern03.java

---
+++ src/patterns/pattern03.java
@@ -0,0 +1,24 @@
+package patterns;
+
+/*This code will generate the pattern in below format
+`1
+ 1 2
+ 1 2 3
+ 1 2 3 4
+ 1 2 3 4 5`
+*/
+
+public class pattern03 {
+    public static void main(String[] args) {
+        pattern03(5);
+    }
+    static void pattern03(int n){
+        for (int rows = 1; rows <=n; rows++) {
+            for (int cols = 1; cols <=rows; cols++) {
+                System.out.print(cols+" ");
+            }
+            System.out.println();
+            
+        }
+    }
+}
