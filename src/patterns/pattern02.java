35bebe80ceea31f6f5430f61e2ecd496f0797d14
Younus-Saberi <younussaberi@gmail.com>
2023-05-10 15:07:33 +0530

    added pattern02.java

--- src/patterns/pattern02.java
+++ src/patterns/pattern02.java
@@ -1,4 +1,27 @@
 package patterns;
 
+/*
+
+This will print the pattern in below format
+
+`* * * * *
+ * * * *
+ * * *
+ * *
+ *`
+
+*/
+
 public class pattern02 {
+    public static void main(String[] args) {
+        pattern02(5);
+    }
+    static void pattern02(int n){
+        for (int rows = 1; rows <= n; rows++) {
+            for (int cols = rows; cols <= n; cols++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
