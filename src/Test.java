97885fa12d01bb79d4a1ad5d977f2f161f11d221
Younus-Saberi <younussaberi@gmail.com>
2023-05-10 14:34:46 +0530

    initial commit

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello from the test.java file");
-
-        for (int i = 0; i < 10; i++) {
-            System.out.println("i = "+i);
-        }
-    }
-}
