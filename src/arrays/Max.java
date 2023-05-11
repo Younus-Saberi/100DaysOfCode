15578254a67f69bed4b02a2b881057b432d13860
Younus-Saberi <younussaberi@gmail.com>
2023-05-11 18:49:45 +0530

    added new array problems

--- src/arrays/Max.java
+++ src/arrays/Max.java
@@ -1,5 +1,19 @@
 package arrays;
 
 public class Max {
+public static void main(String[] args) {
+        System.out.println("\nFind Max in an array");
+        int[] arr={1,2,3,4,5};
+        System.out.println(max(arr));
+    }
 
+    static int max(int[] arr){
+        int max=arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
 }
